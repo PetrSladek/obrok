13c1ea759c431dc731811b50b10fa6f733cffdee
Petr Sladek <petr.sladek@skaut.cz>
2015-08-27 15:31:47 +0200

    fix temp dir for tests in travis

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -11,6 +11,7 @@ class_alias('Tester\Assert', 'Assert');
 date_default_timezone_set('Europe/Prague');
 
 // create temporary directory
+@mkdir( __DIR__ . '/../temp/');
 define('TEMP_DIR', __DIR__ . '/../temp/' . (isset($_SERVER['argv']) ? md5(serialize($_SERVER['argv'])) : getmypid()));
 Tester\Helpers::purge(TEMP_DIR);
 
