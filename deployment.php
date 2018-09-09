c57736391a5fca82c23432f05283873d3e761311
Petr Sládek <petr.sladek@skaut.cz>
2018-09-09 14:54:48 +0200

    vypnutí testů protože stejně nejedou

---
+++ deployment.php
@@ -0,0 +1,35 @@
+<?php
+
+require __DIR__ . '/vendor/autoload.php';
+
+$dotenv = new Dotenv\Dotenv(__DIR__);
+$dotenv->load();
+
+return [
+	'lebeda' => [
+		'remote' => 'ftp://is-obrok19.skauting.cz/',
+		'user' => getenv('FTP_USER'),
+		'password' => getenv('FTP_PASS'),
+
+		'local' => '.',
+		'test' => false,
+
+		'ignore' => "
+		    .git*
+		    .idea
+			app/config/config.local.neon
+			app/config/config.local.*.neon
+			log/*
+			temp/*
+			www/storage/webimages/
+	
+		",
+
+		'allowdelete' => 'yes',
+
+		'purge' => ['temp/cache'],
+	],
+
+	'tempDir' => __DIR__ . '/temp',
+	'colors' => false,
+];
