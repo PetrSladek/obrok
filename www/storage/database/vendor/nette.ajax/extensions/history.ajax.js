925ab3ba15f604027e111b60d77f9d72b89291e5
Petr Sládek <petr.sladek@skaut.cz>
2018-10-22 20:41:13 +0200

    oprava exportu a ajax historie

--- www/storage/database/vendor/nette.ajax/extensions/history.ajax.js
+++ www/storage/database/vendor/nette.ajax/extensions/history.ajax.js
@@ -83,12 +83,12 @@ $.nette.ext('history', {
 	success: function (payload) {
 		var redirect = payload.redirect || payload.url; // backwards compatibility for 'url'
 		if (redirect) {
-			var regexp = new RegExp('//' + window.location.host + '($|/)');
-			if ((redirect.substring(0,4) === 'http') ? regexp.test(redirect) : true) {
-				this.href = redirect;
-			} else {
+			// var regexp = new RegExp('//' + window.location.host + '($|/)');
+			// if ((redirect.substring(0,4) === 'http') ? regexp.test(redirect) : true) {
+			// 	this.href = redirect;
+			// } else {
 				window.location.href = redirect;
-			}
+			// }
 		}
 		if (this.href && this.href != window.location.href) {
 			history.pushState({
