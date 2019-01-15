6176f8e8ce6a0fda6ebc2e9b969cb4641d6ca813
Petr Sládek <petr.sladek@skaut.cz>
2019-01-15 21:14:20 +0100

    oprava formatu

--- app/modules/Database/PaymentsPresenter.php
+++ app/modules/Database/PaymentsPresenter.php
@@ -63,7 +63,7 @@ class PaymentsPresenter extends DatabaseBasePresenter
 	    $frm->addUpload('file', 'XLSX soubor s importem')
 //            ->addRule(Form::MIME_TYPE, 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
             ->setRequired()
-        ->setHtmlAttribute('accept', '.xls,.xlsx,.csv');
+        ->setHtmlAttribute('accept', '.xls,.xlsx');
 
 	    $frm->addSubmit('send', 'Odeslat');
 
