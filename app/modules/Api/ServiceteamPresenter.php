fa58d74c37e21cc32b9cc64e547e6bf2e3e2d55f
Petr /Peggy/ Sládek <petr.sladek@skaut.cz>
2019-05-07 08:31:51 +0200

    Create ServiceteamPresenter.php

---
+++ app/modules/Api/ServiceteamPresenter.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Module\Api\Presenters;
+
+use App\Forms\GroupForm;
+use App\Forms\IGroupFormFactory;
+use App\Model\Entity\Group;
+use App\Model\Entity\Participant;
+use App\Forms\Form;
+use App\Model\Entity\Person;
+use App\Model\Entity\Program;
+use App\Model\Entity\ProgramSection;
+use App\Model\Repositories\GroupsRepository;
+use App\Model\Repositories\ServiceteamRepository;
+use App\Model\Repositories\ProgramsRepository;
+use App\Model\Repositories\ProgramsSectionsRepository;
+use App\Query\ServiceteamQuery;
+use App\Query\ProgramsQuery;
+use App\Query\ProgramsSectionsQuery;
+use App\Services\ImageService;
+use Nette\Utils\AssertionException;
+use Nette\Utils\DateTime;
+use Nette\Utils\Html;
+use App\Services\ImageService;
+use Nette\Utils\Image;
+
+/**
+ * Class ServiceteamPresenter
+ * @package App\Module\Api\Presenters
+ */
+class ServiceteamPresenter extends \Nette\Application\UI\Presenter
+{
+
+    /**
+     * @var ServiceteamRepository
+     * @inject
+     */
+    public $serviceteamRepostitory;
+    
+    /** @var ImageService @inject */
+	  public $images;
+
+   
+    /**
+     * Vrátí avatary ST
+     *
+     * @param null $sectionId
+     * @throws \Nette\Application\AbortException
+     */
+	public function actionAvatars()
+	{
+	    $query = new ServiceteamQuery();
+
+        $serviceteams = $this->serviceteamRepository->fetch($query);
+        
+        $avatars = [];
+        foreach($serviceteams as $serviceteam)
+        {
+          $avatars[] = [
+            'id' => $serviceteam->getId(),
+            'url' => $this->images->getImageUrl($serviceteam->getAvatarUrl(), 800, 800, Image::EXACT, $serviceteam->getAvatarCrop()),
+          ]
+        }
+      
+
+        $this->sendJson($avatars);
+	}
+
+}
